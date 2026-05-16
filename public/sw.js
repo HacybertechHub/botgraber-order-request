3bd9ba0ed1fece1ef6114635321db5045e4dd57d
HACYBERGLOBALTECH <4wg4n5shrv@privaterelay.appleid.com>
2026-05-16 12:12:38 -0700

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,1 @@
+self.addEventListener('install',e=>{e.waitUntil(caches.open('hacyber-v1').then(c=>c.addAll(['/','/admin.html'])))});self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
