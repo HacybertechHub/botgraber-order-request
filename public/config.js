f02dca7a41d3b9d79d5db5734d7e4fab3c24b289
HACYBERGLOBALTECH <4wg4n5shrv@privaterelay.appleid.com>
2026-05-17 04:50:09 -0700

    Create config.js

---
+++ public/config.js
@@ -0,0 +1,23 @@
+export const SITE_CONFIG = {
+  siteName: "HACYBERGLOBALTECH",
+  logo: "/logo.png",
+  
+  // Payment methods - Wise tag already set
+  wiseTag: "@hacyberglobaltech",
+  
+  wallets: {
+    BTC: "35e99VQwk2dYUiBJeKe2wnd73drgFFacPp",
+    ETH: "0x575acfce162dc14fb2f6a7440c0da61c3d5f8a8f",
+    USDT_ERC20: "0x575acfce162dc14fb2f6a7440c0da61c3d5f8a8f",
+    USDT_TRC20: "TBdEnSsWQwZAMKiRe54fPi3d45aJyoXqj6"
+  },
+
+  products: [
+    { id: "bot", name: "HACYBER Premium Bot", price: 99 },
+    { id: "signals", name: "Crypto Signals 1 Month", price: 49 },
+    { id: "ebook", name: "Hacking Guide PDF", price: 29 }
+  ],
+  
+  supportEmail: "support@hacyberhub@mail.com", // CHANGE THIS
+  telegram: "https://t.me/hacyberglobaltech" // CHANGE THIS
+};
